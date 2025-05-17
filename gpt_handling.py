b485bfefabfabfb25ab6f15216820dddea94d50d
윤성연 <s2427@e-mirim.hs.kr>
2025-05-18 00:14:15 +0900

    cohere 연결

--- gpt_handling.py
+++ gpt_handling.py
@@ -1,5 +1,12 @@
-print("하하하하호호호호호")
-print("하핳하ㅏ하하하하하ㅏ222222")
-#ㅎㅇㅊ
-print("제발요 아 제발요")
-print("commit 성공 기원")
+import cohere
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # .env 파일 불러오기
+api_key = os.getenv("COHERE_API_KEY")  # 키 가져오기
+
+co = cohere.Client(api_key)
+
+question = input("고민을 입력하세요 : ")
+response = co.chat(message = question)
+print("상담봇 :", response.text)
